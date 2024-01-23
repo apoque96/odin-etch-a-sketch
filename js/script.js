a7c4593bde2c79080b55f146fa798e8d66ab2a87
apoque96 <dereckcabrerang@gmail.com>
2024-01-23 10:53:29 -0600

    Can paint in canvas. currently only red is available

--- js/script.js
+++ js/script.js
@@ -3,15 +3,27 @@ const canvasWidthInput = document.querySelector("#canvasWidthInput");
 const canvasHeightInput = document.querySelector("#canvasHeightInput");
 const btnChangeSize = document.querySelector("#btnChangeSize");
 
+let mouseIsPressed = false;
+
+document.addEventListener('mousedown', function(){
+    mouseIsPressed = true;
+})
+document.addEventListener('mouseup', function(){
+    mouseIsPressed = false;
+})
+
 createCanvas(16, 16);
 btnChangeSize.addEventListener('click', function(){
     createCanvas(canvasWidthInput.value, canvasHeightInput.value)})
 
 function createCanvas(n, m){
+    if(n > 100 || m > 100){
+        alert("The width and height can be at max 100. Please enter a value under 100");
+        return;
+    }
     while(canvas.firstChild){
         canvas.removeChild(canvas.firstChild);
     }
-    canvas.style.aspectRatio = n + "/" + m;
     for(let i = 0; i < m; i++){
         const div = document.createElement('div');
         div.classList.toggle("pixelContainer");
@@ -22,6 +34,12 @@ function createCanvas(n, m){
             anotherDiv.classList.toggle("pixel");;
             anotherDiv.style.width = (100 / n) + "%";
             anotherDiv.style.height = "100%";
+            anotherDiv.addEventListener('mouseenter', function(e){
+                if(mouseIsPressed){
+                    e.target.style.backgroundColor = "red";
+                    e.target.style.opacity = "1.0";
+                }
+            })
             if((i + j) % 2 != 0){
                 anotherDiv.style.backgroundColor = "gray";
                 anotherDiv.classList.toggle("gray");
