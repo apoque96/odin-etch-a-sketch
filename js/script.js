b2641f1eca9c489c202e16359236d67c8f97b36e
apoque96 <dereckcabrerang@gmail.com>
2024-01-22 22:07:01 -0600

    Added the option to change the size of the canvas

--- js/script.js
+++ js/script.js
@@ -1,20 +1,33 @@
 const canvas = document.querySelector("#canvas");
+const canvasWidthInput = document.querySelector("#canvasWidthInput");
+const canvasHeightInput = document.querySelector("#canvasHeightInput");
+const btnChangeSize = document.querySelector("#btnChangeSize");
 
 createCanvas(16, 16);
+btnChangeSize.addEventListener('click', function(){
+    createCanvas(canvasWidthInput.value, canvasHeightInput.value)})
 
 function createCanvas(n, m){
-    for(let i = 0; i < n; i++){
+    while(canvas.firstChild){
+        canvas.removeChild(canvas.firstChild);
+    }
+    canvas.style.aspectRatio = n + "/" + m;
+    for(let i = 0; i < m; i++){
         const div = document.createElement('div');
-        div.classList.toggle("pixel");
+        div.classList.toggle("pixelContainer");
         div.style.width = "100%";
         div.style.height = (100 / m) + "%";
-        for(let j = 0; j < m; j++){
+        for(let j = 0; j < n; j++){
             const anotherDiv = document.createElement("pixel");
-            anotherDiv.classList.toggle("pixel");
+            anotherDiv.classList.toggle("pixel");;
             anotherDiv.style.width = (100 / n) + "%";
             anotherDiv.style.height = "100%";
             if((i + j) % 2 != 0){
                 anotherDiv.style.backgroundColor = "gray";
+                anotherDiv.classList.toggle("gray");
+            }
+            else{
+                anotherDiv.classList.toggle("white");
             }
             div.appendChild(anotherDiv);
         }
